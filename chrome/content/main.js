65466bab36960864191db796be30c44207a61bc8
Adrián Arroyo <adrian@divelonline.no-ip.org>
2014-02-01 22:22:07 +0100

    Extension manager

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -8,4 +8,8 @@ window.addEventListener("load",function(){
 	menuconfiguration.addEventListener("command",function(){
 		window.open("chrome://global/content/config.xul","Advanced configuration","chrome, width=600, height=300");
 	});
+	var menuextension=document.getElementById("menu-extension");
+	menuextension.addEventListener("command",function(){
+		window.open("chrome://mozapps/content/extensions/extensions.xul","Extension manager","chrome, width=600, height=300");
+	});
 });
